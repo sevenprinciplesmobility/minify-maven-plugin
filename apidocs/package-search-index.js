5924d335f9b1e9906cc5fbeda7b5a4e19a602b3b
Sasu <git@sasu.net>
2024-12-11 18:02:23 +0100

    Site checkin for project com.7p-group:minify-maven-plugin

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.samaxes.maven.minify.common"},{"l":"com.sevenprinciplesmobility.maven.minify"},{"l":"com.sevenprinciplesmobility.maven.minify.common"},{"l":"com.sevenprinciplesmobility.maven.minify.plugin"}];updateSearchResults();
